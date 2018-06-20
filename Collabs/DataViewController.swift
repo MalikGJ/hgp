51634c752da6e59f037e5345ee6b83e0171e2dd3
Malik Garcia <malikgarcia@maliks-macbook-air.local>
2018-06-20 10:35:40 -0700

    Initial Commit

---
+++ Collabs/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Collabs
+//
+//  Created by Malik Garcia on 6/20/18.
+//  Copyright © 2018 Malik Garcia. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
